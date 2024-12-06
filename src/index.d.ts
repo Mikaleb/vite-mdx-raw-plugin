583ce3ed2301861e6adfbdfa485e08188ca15795
Mikaleb <mikaleb@live.fr>
2024-12-06 22:54:31 +0100

    fix(naming): all naming and more details in readme

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+import { Plugin } from "vite";
+
+declare const viteMdxRawPlugin: () => Plugin;
+
+export default viteMdxRawPlugin;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import type { Plugin } from "vite";
 
-export default function mdxRawPlugin(): Plugin {
+/**
+ * Returns a Vite plugin that transforms MDX files by wrapping them in an export statement and JSON-stringifying the code.
+ *
+ * @returns The Vite plugin instance.
+ */
+export default function viteMdxRawPlugin(): Plugin {
   return {
-    name: "vite-plugin-mdx-raw",
+    name: "vite-mdx-raw-plugin",
     transform(code, id) {
       if (id.endsWith(".mdx")) {
         return {
-          code: `export default ${JSON.stringify(code)}`,
-          map: null,
+          code: `export default ${JSON.stringify(code)
+            .replace(/\u2028/g, "\\u2028")
+            .replace(/\u2029/g, "\\u2029")}`,
         };
       }
     },
